434af595fd034f59e3620cdea19e4e32312d8cd8
Projeet <68896314+ProjeetBhaumik@users.noreply.github.com>
2026-09-24 13:26:11 +0100

    Add: coin-change - submission-8

---
+++ Data Structures & Algorithms/coin-change/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        cache = {}
+        def dfs(i,remaining):
+            if remaining == 0:
+                return 0
+            if (i,remaining) in cache:
+                return cache[(i,remaining)]
+  
+            if remaining < 0 or i == len(coins):
+                return float('inf')
+            take = 1 + dfs(i,remaining - coins[i])
+            skip = dfs(i+1,remaining)
+            cache[(i,remaining)] = min(skip,take)
+
+            return cache[(i,remaining)]
+        
+        ans = dfs(0,amount) 
+        return -1 if ans == float('inf') else ans
